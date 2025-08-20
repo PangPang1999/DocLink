f60433b046710699ac73d97f5fb7154256fb9587
Yi Miao <ppp_melody@163.com>
2025-08-20 17:07:14 +0800

    init

---
+++ src/main/java/com/pang/rag_demo/ChatController.java
@@ -0,0 +1,30 @@
+package com.pang.rag_demo;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.awt.SystemColor.text;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder, PgVectorStore vectorStore) {
+        this.chatClient = builder
+                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
+                .build();
+    }
+
+    @GetMapping("/")
+    public String chat() {
+        return chatClient.prompt()
+                .user("What are the specific impacts of Trump’s tariff policy on the US agriculture and durable manufacturing sectors.")
+                .call()
+                .content();
+    }
+
+}
---
+++ src/main/java/com/pang/rag_demo/IngestionService.java
@@ -0,0 +1,44 @@
+package com.pang.rag_demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.document.DocumentReader;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class IngestionService implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(IngestionService.class);
+    private final VectorStore vectorStore;
+
+    @Value("classpath:/docs/article.pdf")
+    private Resource resource;
+
+    public IngestionService(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // 1) 用 Tika 读取文档（支持 txt / doc / docx / pptx / html / pdf 等多种格式）
+        DocumentReader reader = new TikaDocumentReader(resource);
+
+        // 2) 切分为 chunk（可根据需要调参：chunkSize, overlap…）
+        TextSplitter splitter = new TokenTextSplitter();
+        // 例如自定义：
+        // TextSplitter splitter = new TokenTextSplitter(800, 200, 4);
+
+        // 3) 写入向量库
+        vectorStore.accept(splitter.apply(reader.get()));
+
+        log.info("VectorStore loaded with data from {}!", resource.getFilename());
+    }
+}
